8edf2918d43ee42ccd46a5007554a3151435c8c4
Stefan Kofler <grafele@gmail.com>
2019-08-02 11:01:00 +0200

    Initial Commit

---
+++ SpeedTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedTest
+//
+//  Created by Stefan Kofler on 02.08.19.
+//  Copyright © 2019 QuickBird Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
